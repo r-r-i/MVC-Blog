ee4a092a676ba5fa91f5edf07e804773c2e68eba
brake <riaconoo@icloud.com>
2022-08-30 13:16:16 +1000

    populated models

---
+++ models/Blog.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const User = require('./User');
+class Blog extends Model { }
+
+Blog.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        comment: {
+            type: DataTypes.STRING,
+        },
+        user_id: {
+            type: DataTypes.INTERGER,
+            references: {
+                model: User,
+                key: 'id'
+            },
+            unique: false,
+        }
+    },
+    {
+        sequelize: sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        modelname: 'Blog'
+    }
+);
+
+module.exports = Blog;
